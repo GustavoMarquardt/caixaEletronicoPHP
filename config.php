3c6d8cfd2c6ed25f8bc48b8f74854240585c0033
Gustavo Marquardt <gustavo.marquardt@unifei.edu.br>
2022-03-08 18:50:08 -0300

    second commit

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	$pdo = new PDO("mysql:dbname=projeto_caixaeletronico;host=localhost", "root", "root");
+	$pdo = new PDO("mysql:dbname=projeto_caixaeletronico;host=localhost", "root", "");
 } catch(PDOException $e) {
 	echo "ERRO: ".$e->getMessage();
 	exit;
